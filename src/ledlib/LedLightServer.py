74e02b43b8bcdc375cb23c50073bb3f9e292115a
triphop <el.triphop@gmail.com>
2015-10-26 20:13:27 -0400

    Added some flexibility and smarts to the ledlight server

--- src/ledlib/LedLightServer.py
+++ src/ledlib/LedLightServer.py
@@ -11,6 +11,8 @@ from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import LineReceiver
 from ServerCommand import ServerCommand
 
+# Protocol for the led light
+# Processes the line and if parses ok, hands off to command handler
 class LedLightProtocol(LineReceiver):
   delimiter = '\n'
   def lineReceived(self, line):
@@ -18,13 +20,29 @@ class LedLightProtocol(LineReceiver):
     if hasattr(command, 'errmsg'):
       self.sendLine("nok:"+command.errmsg)
     else:
-      self.sendLine("ok")
+      if self.commandHandler.handleCommand(command)==0:
+        self.sendLine("ok")
+      else: 
+        self.sendLine("nok:"+self.commandHandler.errmgs)
+        
 
+# Hooks up the protocol to the server factory
 class LedLightFactory(ServerFactory):
-    # This will be used by the default buildProtocol to create new protocols:
-    protocol = LedLightProtocol
+    def __init__(self, commandHandler):
+      self.protocol = LedLightProtocol
+      self.protocol.commandHandler = commandHandler
 
+# Null command handler - does nothing really - just print the command
+class NullCommandHandler:
+  def handleCommand(self, command):
+    print "Handling command: "+command.comname
+    return 0
+
+# This function does the deed - runs the server with the handler
+def runServer(port, commandHandler):
+  reactor.listenTCP(port, LedLightFactory(commandHandler))
+  reactor.run()
+  
 if __name__ == "__main__":
   print "Running now"
-  reactor.listenTCP(8123, LedLightFactory())
-  reactor.run()
+  runServer(8123,NullCommandHandler())
