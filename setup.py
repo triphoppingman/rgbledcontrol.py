ad1eb0e0bbe0c5ded21a2d5b16d55b32fcffe126
bruce <el.triphop@gmail.com>
2016-09-14 21:01:53 -0400

    Updated the setup.py.   This is really my first project so go easy!

--- setup.py
+++ setup.py
@@ -5,8 +5,30 @@ setup(
     version='0.1',
     packages=['src', 'src.ledlib'],
     url='https://github.com/triphoppingman/rgbledcontrol.py',
-    license='',
-    author='bruce',
-    author_email='',
-    description=''
+    license='Apache',
+    author='Bruce McDonald',
+    author_email='el.triphop@gmail.com',
+    description='Library to manipulate two kinds of WIFI RGB lights',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='RGB WIFI LED',
+    install_requires=['twisted', 'PyParsing']
 )
